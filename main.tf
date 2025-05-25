7b38672a286ff12eebb1360360608c8b4f5edf52
Komal Raja <KRaja@TechStyle.com>
2025-05-25 13:37:40 +0300

    addedprojectid

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "storage" {
   source          = "./storage"
   bucket_name     = var.bucket_name
-  project         = var.project_id
+  project_id      = var.project_id
   bucket_location = var.bucket_location
 
 }
