11628fe1aa49121d42e96ae2e447b5337e9cde26
komalazram <komal.azram@gmail.com>
2025-06-10 19:15:58 +0300

    refactoring

--- terraform/storage/main.tf
+++ terraform/storage/main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "storage-buckets" {
   count         = length(var.bucket_names)
-  name          = var.bucket_names
+  name          = var.bucket_names[count.index]
   location      = var.bucket_location
   project       = var.project_id
   force_destroy = true
